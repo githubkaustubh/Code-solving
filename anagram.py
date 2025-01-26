9c1c065d67ee0367891d6f1b8433e74eb8d1a857
githubkaustubh <126224728+githubkaustubh@users.noreply.github.com>
2025-01-26 18:55:48 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,17 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        count1 = {}
-        count2 = {}
+        countmapS = {}
+        countmapT = {}
 
         if len(s) != len(t):
             return False
         
         for i in range (len(s)):
-            count1[s[i]] = 1 + count1.get(s[i],0)
-            count2[t[i]] = 1 + count2.get(t[i], 0)
-        for j in count1 : 
-            if count1[j] != count2.get(j,0):
+            countmapS[s[i]] = 1 + countmapS.get(s[i],0)
+            countmapT[t[i]] = 1 + countmapT.get(t[i], 0)
+        for j in countmapS : 
+            if countmapS[j] != countmapT[j]:
                 return False 
         return True
         
