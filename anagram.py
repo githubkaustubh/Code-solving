cdc8999e8e8e07a8a4825d6577ad13b2f7d04d51
githubkaustubh <126224728+githubkaustubh@users.noreply.github.com>
2025-01-26 18:41:25 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -8,9 +8,10 @@ class Solution:
             return False
         
         for i in range (len(s)):
-            count1[s[i]] +  = count1[s[i]]
-            count2[t[i]] + =  count2[t[i]]
-        for j in count2 : 
-            if count1[j] != count2[j]:
+            count1[s[i]] = 1 + count1.get(s[i],0)
+            count2[t[i]] = 1 + count2.get(t[i], 0)
+        for j in count1 : 
+            if count1[j] != count2.get(j,0):
                 return False 
         return True
+        
